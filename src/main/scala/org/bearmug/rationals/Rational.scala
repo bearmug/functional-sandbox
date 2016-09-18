34ad6b4eed2c672bb2c920f68aad4e14a1b0a06e
bearmug <pavel.fadeev@gmail.com>
2016-09-19 01:20:11 +0300

    #2 shorten Scala version avoiding object fields re-declaration

--- src/main/scala/org/bearmug/rationals/Rational.scala
+++ src/main/scala/org/bearmug/rationals/Rational.scala
@@ -3,14 +3,12 @@ package org.bearmug.rationals
 /**
  * Functional rational numbers implementation. Inspired by 'Programming in Scala' book.
  */
-class Rational(n: Int, d: Int) {
-  val number = n
-  val denominator = d
+class Rational(val n: Int, val d: Int) {
 
   override def toString: String = s"$n/$d"
   def unary_- = Rational(-n, d)
-  def *(o: Rational) = Rational(n * o.number, d * o.denominator)
-  def /(o: Rational) = Rational(n * o.denominator, d * o.number)
+  def *(o: Rational) = Rational(n * o.n, d * o.d)
+  def /(o: Rational) = Rational(n * o.d, d * o.n)
 }
 
 object Rational {
