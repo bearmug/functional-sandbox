bc56a0a1b95890086ff85d72cf5cd355d0a73295
bearmug <pavel.fadeev@gmail.com>
2016-09-19 22:58:49 +0300

    #2 new methods for rationals Scala class

--- src/main/scala/org/bearmug/rationals/Rational.scala
+++ src/main/scala/org/bearmug/rationals/Rational.scala
@@ -9,9 +9,19 @@ class Rational(val n: Int, val d: Int) {
   def unary_- = Rational(-n, d)
   def *(o: Rational) = Rational(n * o.n, d * o.d)
   def /(o: Rational) = Rational(n * o.d, d * o.n)
+  def +(o: Rational) = Rational(n * o.d + o.n * d, d * o.d)
+  def -(o: Rational) = Rational(n * o.d - o.n * d, d * o.d)
+  def ==(o: Rational) = this - o match {case Rational(in, _) => in == 0}
+  def >(o: Rational) = this - o match {case Rational(in, _) => in > 0}
+  def <(o: Rational) = this - o match {case Rational(in, _) => in < 0}
+  def >=(o: Rational) = this > o || this == o
+  def <=(o: Rational) = this < o || this == o
+  def min(o: Rational) = if (this <= o) this else o
+  def max(o: Rational) = if (this >= o) this else o
 }
 
 object Rational {
   def apply(n: Int, d: Int): Rational = new Rational(n, d)
   def apply(n: Int): Rational = new Rational(n, 1)
+  def unapply(r: Rational): Option[(Int, Int)] = Some(r.n: Int, r.d: Int)
 }
