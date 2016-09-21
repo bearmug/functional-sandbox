817f680332a9a053a282e05ef943aff40f1ff91b
bearmug <pavel.fadeev@gmail.com>
2016-09-22 01:31:35 +0300

    #9 LIS Scala code and tests submission

--- src/test/scala/org/bearmug/algo/LISubsequenceSuite.scala
+++ src/test/scala/org/bearmug/algo/LISubsequenceSuite.scala
@@ -7,7 +7,31 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class LISubsequenceSuite extends FunSuite {
 
-  test("stub implementation works") {
-    assert(new LISubsequence(List()).res == 1)
+  test("empty subsequence for empty list") {
+    assert(new LISubsequence(List()).length == 0)
+  }
+
+  test("subsequence length for List(1) is 1") {
+    assert(new LISubsequence(List(1)).length == 1)
+  }
+
+  test("subsequence length for List(1, 2, 3) is 3") {
+    assert(new LISubsequence(List(1, 2, 3)).length == 3)
+  }
+
+  test("subsequence length for List(3, 2, 1) is 1") {
+    assert(new LISubsequence(List(1, 2, 3)).length == 3)
+  }
+
+  test("subsequence length for List(3, 1, 2) is 2") {
+    assert(new LISubsequence(List(3, 1, 2)).length == 2)
+  }
+
+  test("subsequence length for List(3, 1, 2, 4) is 3") {
+    assert(new LISubsequence(List(3, 1, 2, 4)).length == 3)
+  }
+
+  test("subsequence length for List(3, 1, 2, 20, 4, 5) is 4") {
+    assert(new LISubsequence(List(3, 1, 2, 20, 4, 5)).length == 4)
   }
 }
