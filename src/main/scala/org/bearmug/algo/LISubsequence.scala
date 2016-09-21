25b9ebbf69e6e15b1b38bb23812eaf95e53c1813
bearmug <pavel.fadeev@gmail.com>
2016-09-22 01:06:23 +0300

    #9 stub implementation for LIS Scala version

---
+++ src/main/scala/org/bearmug/algo/LISubsequence.scala
@@ -0,0 +1,10 @@
+package org.bearmug.algo
+
+/**
+  * Class to see if Scala code for [[https://en.wikipedia.org/wiki/Longest_increasing_subsequence LIS]]
+  * could be clean and efficient.
+  */
+class LISubsequence(l: List[Int]) {
+
+  lazy val res = 1
+}
---
+++ src/test/scala/org/bearmug/algo/LISubsequenceSuite.scala
@@ -0,0 +1,13 @@
+package org.bearmug.algo
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class LISubsequenceSuite extends FunSuite {
+
+  test("stub implementation works") {
+    assert(new LISubsequence(List()).res == 1)
+  }
+}
